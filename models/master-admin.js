9c1e0b6f8565d27fff0c41e8cb0f1e454a7aec30
rahulkacha <rahulbkacha@gmail.com>
2022-04-05 22:51:03 +0530

    added a master-admin model and completed all the front end for master admin pages.

---
+++ models/master-admin.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Society = require("./society");
+
+// mongoose.connect("mongodb://localhost:27017/smartSocietyDB");
+
+const masterAdminSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true }, // will be validated during the request
+  password: { type: String, required: true },
+});
+
+const MasterAdmin = mongoose.model("MasterAdmin", masterAdminSchema);
+
+const masterAdmin = new MasterAdmin({
+  name: "rahul kacha",
+  email: "rahulbkacha@gmail.com",
+  password: "password",
+});
+
+// masterAdmin.save((err, obj) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(obj);
+//   }
+// });
--- routes/masterAdminRoutes.js
+++ routes/masterAdminRoutes.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+// LOGIN
+
+router
+  .route("/master/admin/login")
+  .get(function (req, res) {
+    res.render("master-admin-pages/master-admin-login");
+  })
+  .post(function (req, res) {
+    console.log(req.body);
+    res.redirect("/master/admin/manage-societies");
+  });
+
+// GOOGLE OAUTH
+router
+  .route("/master/admin/auth/google")
+
+  .get(function (req, res) {
+    res.send("master admin google auth triggered.");
+  });
+
 // ADD A SOCIETY
 router
   .route("/master/admin/add-society")
